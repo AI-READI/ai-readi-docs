cb0e0e2e97c9608b8bebb9b75cd69414c2bbb264
aydawka <aydan.gasimova2@gmail.com>
2024-03-18 00:29:26 -0700

    style: 🎨 fix format

--- src/components/SnapshotAnalysis.jsx
+++ src/components/SnapshotAnalysis.jsx
@@ -7,15 +7,15 @@ import styles from './HomepageFeatures.module.css';
 const StatsList = [
   {
     heading: '4,000',
-    text: 'Participants anticipated <br /> in the study',
+    text: 'Participants anticipated in the study',
   },
   {
     heading: '15+',
-    text: 'Data types to be collected <br /> (vitals, electrocardiogram, etc.)',
+    text: 'Data types to be collected (vitals, electrocardiogram, etc.)',
   },
   {
     heading: '8',
-    text: `Research institutions <br /> involved`,
+    text: `Research institutions involved`,
   },
 ];
 
@@ -23,7 +23,7 @@ export default function SnapshotAnalysis() {
   return (
     <section className={styles.features}>
       <div className={styles.snapshot}>
-        <h1 className="mb-2 text-3xl font-bold tracking-tight sm:text-4xl">
+        <h1 className="mb-2 text-3xl font-bold tracking-tight sm:text-3xl">
           Snapshot of the AI-READI project
         </h1>
 
@@ -31,20 +31,14 @@ export default function SnapshotAnalysis() {
           Some key numbers from the project
         </p>
 
-        <div className="flex justify-center">
-          <div className="flex flex-row">
-            {StatsList.map((stat) => (
-              <div
-                key={stat.heading}
-                className="flex flex-col items-center
-                justify-start p-3 text-center"
-              >
-                <dt className="text-5xl font-bold text-sky-600">{stat.heading}</dt>
+        <div className={styles.stat}>
+          {StatsList.map((stat) => (
+            <dl className="max-w-[25rem]" key={stat.heading}>
+              <dt className="text-5xl font-bold text-sky-600">{stat.heading}</dt>
 
-                <dd className="text-lg font-medium m-0 text-gray-700">{stat.text}</dd>
-              </div>
-            ))}
-          </div>
+              <dd className="text-lg font-medium m-0 text-gray-700">{stat.text}</dd>
+            </dl>
+          ))}
         </div>
       </div>
     </section>
