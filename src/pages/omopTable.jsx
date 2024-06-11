175d3e10c37779cf8ff5d90f1a1add56fcd8564c
aydawka <aydan.gasimova2@gmail.com>
2024-06-11 16:38:02 -0700

    style: 🎨 format

--- src/pages/omopTable.jsx
+++ src/pages/omopTable.jsx
@@ -2,15 +2,12 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 
-// eslint-disable-next-line import/no-unresolved
-import Layout from '@theme/Layout';
-
 import StructureTable from '../components/StructureTable';
 
 export default function OmopTable() {
   return (
-    <Layout title="" description="Documentation for the AI-READI dataset">
+    <div>
       <StructureTable />
-    </Layout>
+    </div>
   );
 }
