7736ead7f78bca3ad52c42bfd98ab632b8d5be36
aydawka <aydan.gasimova2@gmail.com>
2024-06-11 13:06:24 -0700

    chore: 🔨 redirect to a new page

--- src/components/DirectOmopTable.jsx
+++ src/components/DirectOmopTable.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 function DirectOmopTable() {
   useEffect(() => {
+    // Open OMOP Table in a new tab
     window.open('/omopTable', '_blank');
-  }, []);
 
-  return <h2>Redirecting...</h2>;
+    // Redirect the current page to the documentation
+    window.location.replace('/docs/1/dataset/clinical-data/OMOP-Clinical-Data-Structure');
+  }, []);
 }
 
 export default DirectOmopTable;
