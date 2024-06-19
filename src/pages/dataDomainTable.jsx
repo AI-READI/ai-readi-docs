bc40b26994cb2baf93e0a29d0fa5cc62f9f49134
Aydan Gasimova <62059163+Aydawka@users.noreply.github.com>
2024-06-19 13:14:36 -0700

    chore: 🔨 redirect omop mapping table to new page (#19)
    * chore: 🔨 move omop mapping table to new page
    
    * style: 🎨 fix margin
    
    * style: 🎨 fix margin
    
    * style: 🎨 fix margin for header
    
    * chore: 🔨 update table header
    
    * chore: 🔨 separate page
    
    * chore: 🔨 update omop table
    
    * style: 🎨 font size
    
    * fix: 🐛 browseronly issue
    
    * fix: 🐛 add redirection
    
    * fix: 🐛 fix table style
    
    * chore: 🔨 redirect to a new page
    
    * chore: 🔨 redirect to a new page
    
    * style: 🎨 format
    
    * style: 🎨 format
    
    * style: 🎨 fix the table cell width
    
    * style: 🎨 format
    
    * style: 🎨 format
    
    * style: 🎨 format
    
    * style: 🎨 format
    
    * fix: 🐛 change sidebar header name for table
    
    * chore: 🔨 add data domain table as separate
    
    * chore: 🔨 set file processing table to a new page
    
    * chore: 🔨 direct to the page

---
+++ src/pages/dataDomainTable.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/button-has-type */
+import React from 'react';
+
+import ClinicalLabTable from '../components/ClinicalLabTable';
+
+export default function dataDomainTable() {
+  return (
+    <div>
+      <ClinicalLabTable />
+    </div>
+  );
+}
---
+++ src/pages/fileProcessingTable.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/button-has-type */
+import React from 'react';
+
+import FileProcessingTable from '../components/FileProcessingTable';
+
+export default function fileProcessingTable() {
+  return (
+    <div>
+      <FileProcessingTable />
+    </div>
+  );
+}
