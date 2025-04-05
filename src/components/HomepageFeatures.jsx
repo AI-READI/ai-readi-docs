ca9d3f1db8efaa133d7bfa2670f1ebc6efabf13a
Sanjay Soundarajan <sanjay.16@live.com>
2025-04-04 20:51:20 -0700

    feat: ✨ add banner (#26)
    * feat: ✨ add banner
    
    * fix: 🐛 banner theme
    
    * fix: 🐛 typo
    
    ---------
    
    Co-authored-by: aydawka <aydan.gasimova2@gmail.com>

--- src/components/HomepageFeatures.jsx
+++ src/components/HomepageFeatures.jsx
@@ -47,42 +47,44 @@ const StatsList = [
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.second}>
-      <div>
-        <h1 className="header hero__title tracking-tight sm:text-3xl text-center">
-          Generating Data to Boost Future AI-driven Research in Type 2 Diabetes
-        </h1>
-        <p
-          className="mt-6 pl-[2rem] pr-9 mb-4 text-xl max-w-[70rem] text-justify
+    <div className="bg-[#e0f2fe]">
+      <section className={styles.second}>
+        <div>
+          <h1 className="header hero__title tracking-tight sm:text-3xl text-center">
+            Generating Data to Boost Future AI-driven Research in Type 2 Diabetes
+          </h1>
+          <p
+            className="mt-6 pl-[2rem] pr-9 mb-4 text-xl max-w-[70rem] text-justify
         font-medium text-slate-600"
-        >
-          This is the documentation for the AI-READI dataset called &quot;Flagship Dataset of Type 2
-          Diabetes from the AI-READI Project&quot;. AI-READI is one of the data generation project
-          of the National Institutes of Health funded Bridge2AI Program. One of the major goals of
-          AI-READI is to collect and share a multimodal, AI-ready dataset for studying salutogenesis
-          in Type 2 Diabetes Mellitus.
-        </p>
-      </div>
-
-      <div className={styles.snapshot}>
-        <h1 className="snapshot mb-2 text-3xl font-bold tracking-tight sm:text-3xl">
-          Snapshot of the AI-READI dataset
-        </h1>
-
-        <p className="snapshot mb-12 text-xl font-medium text-slate-600">
-          Key numbers from the current version of the dataset (v2.0.0)
-        </p>
-
-        <div className={styles.stat}>
-          {StatsList.map((stat) => (
-            <dl className="max-w-[17rem] flex flex-col gap-[1rem]" key={stat.text}>
-              <dt className="text-5xl font-bold text-sky-600">{stat.heading}</dt>
-
-              <dd className="text-lg font-medium m-0 text-gray-700">{stat.text}</dd>
-            </dl>
-          ))}
+          >
+            This is the documentation for the AI-READI dataset called &quot;Flagship Dataset of Type
+            2 Diabetes from the AI-READI Project&quot;. AI-READI is one of the data generation
+            project of the National Institutes of Health funded Bridge2AI Program. One of the major
+            goals of AI-READI is to collect and share a multimodal, AI-ready dataset for studying
+            salutogenesis in Type 2 Diabetes Mellitus.
+          </p>
         </div>
-      </div>
-    </section>
+
+        <div className={styles.snapshot}>
+          <h1 className="snapshot mb-2 text-3xl font-bold tracking-tight sm:text-3xl">
+            Snapshot of the AI-READI dataset
+          </h1>
+
+          <p className="snapshot mb-12 text-xl font-medium text-slate-600">
+            Key numbers from the current version of the dataset (v2.0.0)
+          </p>
+
+          <div className={styles.stat}>
+            {StatsList.map((stat) => (
+              <dl className="max-w-[17rem] flex flex-col gap-[1rem]" key={stat.text}>
+                <dt className="text-5xl font-bold text-sky-600">{stat.heading}</dt>
+
+                <dd className="text-lg font-medium m-0 text-gray-700">{stat.text}</dd>
+              </dl>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
---
+++ src/theme/Footer/Layout/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export default function FooterLayout({ style, links, logo, copyright }) {
+  return (
+    <footer
+      className={clsx('footer relative', {
+        'footer--dark': style === 'dark',
+      })}
+    >
+      <div className="container container-fluid">
+        {links}
+
+        {(logo || copyright) && (
+          <div className="footer__bottom text--center">
+            {logo && <div className="margin-bottom--sm">{logo}</div>}
+            {copyright}
+          </div>
+        )}
+      </div>
+
+      <div className="fixed bottom-0 inset-x-0 z-50 w-screen bg-sky-300 p-2 text-center text-sm text-gray-900 max-sm:bottom-2">
+        This repository is under review for potential modification in compliance with Administration
+        directives.
+      </div>
+    </footer>
+  );
+}
